5df603f75c17a41ee1f07775c90ed4438a20e06f
FoxWareFX <foxwarefx@gmail.com>
2021-02-21 17:30:24 +0200

    Commented Source

--- index.js
+++ index.js
@@ -22,19 +22,23 @@ const keyType = {
     }
 }
 
+// Easter egg
 function ee() {
     show(eePhrases[rand(0, eePhrases.length - 1)], "linear-gradient(50deg, #FF0000, #FF9900)");
 }
 
+// Random
 function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+// Boolean Random
 function brand() {
     return rand(0, 1);
 }
 
-function calcSumStr(len) {
+// Calculates the string that his summed digits are divisible by 7
+function calcMod7(len) {
     let str = "0".repeat(len);
 
     for(let i = 0, p = 0, sum = 7 * rand(1, len); sum > 0; i++, p = i % len) {
@@ -47,10 +51,22 @@ function calcSumStr(len) {
     return str;
 }
 
+/*
+ * Gen Win95 Retail
+ * NNN-MMMMMMM
+ * NNN = any number from 0 to 999
+ * MMMMMMM = the sum of the digits MUST be divisible by 7
+ */
 function gO95ret() {
-    return rand(0, 999).toString().padStart(3, '0') + "-" + calcSumStr(7);
+    return rand(0, 999).toString().padStart(3, '0') + "-" + calcMod7(7);
 }
 
+/*
+ * Gen Win95 Retail
+ * NNN-MMMMMMM
+ * NNN = any number from 0 to 999 (except 333, 444, 555 ... 999)
+ * MMMMMMM = the sum of the digits MUST be divisible by 7
+ */
 function gW95ret() {
     let seg1;
 
@@ -58,32 +74,53 @@ function gW95ret() {
         seg1 = rand(0, 999);
     } while(seg1 === 333 || seg1 === 444 || seg1 === 555 || seg1 === 666 || seg1 === 777 || seg1 === 888 || seg1 === 999);
 
-    return seg1.toString().padStart(3, '0') + "-" + calcSumStr(7);
+    return seg1.toString().padStart(3, '0') + "-" + calcMod7(7);
 }
 
+/*
+ * Gen Office95 OEM
+ * NNNNN-OEM-MMMMMMM-PPPPP
+ * MMMMMZ = the sum of the digits MUST be divisible by 7
+ * NNNNN, PPPPP = any number from 0 to 99999
+ */
 function gO95oem() {
-    return rand(0, 99999).toString().padStart(5, '0') + "-OEM-" + calcSumStr(7) + "-" + rand(0, 99999).toString().padStart(5, '0');
+    return rand(0, 99999).toString().padStart(5, '0') + "-OEM-" + calcMod7(7) + "-" + rand(0, 99999).toString().padStart(5, '0');
 }
 
+/*
+ * Gen Win95 OEM
+ * DDDYY-OEM-0MMMMMZ-NNNNN
+ * DDD = Day of the year (1-366)
+ * YY = year (95-02)
+ * 0 = the first digit of the mod7 check MUST be 0
+ * MMMMMZ = the 2-5 digits can be any number (except to the last, MUST NOT be 0), the sum of the digits MUST be divisible by 7
+ * NNNNN = any number from 0 to 99999
+ */
 function gW95oem() {
     let seg1;
 
     do {
-        seg1 = calcSumStr(6);
+        seg1 = calcMod7(6);
     } while(seg1.endsWith("0"));
 
     return rand(1, 366).toString().padStart(3, '0') + (brand()? rand(95, 99) : rand(0, 2)).toString().padStart(2, '0') + "-OEM-0" + seg1 + "-" + rand(0, 99999).toString().padStart(5, '0');
 }
 
+// Generate the key
 function gen(prod, ver) {
+    /*
+     * If it was not initialized, removes the class 'initField' so the code is black and not gray
+     */
     if(!init) {
         keyField.removeClass(initField);
         init = true;
     }
 
+    // Takes the function from the keyType 'enum'
     keyField.val(keyType[prod][ver]());
 }
 
+// Show a notification
 let presses = 0;
 function show(text, bg) {
     note.html(text);
@@ -93,6 +130,10 @@ function show(text, bg) {
     presses++;
 
     setTimeout(function () {
+        /*
+         * removes the class only if the button was not pressed more times.
+         * necessary for not make the note go up and down.
+         */
         if(presses <= 1)
             note.removeClass(noteVisible);
 
@@ -100,6 +141,10 @@ function show(text, bg) {
     }, 2000);
 }
 
+/*
+ * Copy the code from the KEY_FIELD to the clipboard,
+ * showing a notification if was copied successfully.
+ */
 function copy() {
     if(init) {
         navigator.clipboard.writeText(keyField.val());
