a43e197de8492dea6291109b29ddd360b3f29038
FoxWareFX <foxwarefx@gmail.com>
2021-02-22 16:25:32 +0200

    Small changes

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ function gW95ret() {
 /*
  * Gen Office95 OEM
  * NNNNN-OEM-MMMMMMM-PPPPP
- * MMMMMZ = the sum of the digits MUST be divisible by 7
+ * MMMMMMM = the sum of the digits MUST be divisible by 7
  * NNNNN, PPPPP = any number from 0 to 99999
  */
 function gO95oem() {
